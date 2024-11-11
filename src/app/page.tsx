080ed93fc3dcf0e87720261a7a20ff9d3eff629f
alishba66 <164622140+alishba66@users.noreply.github.com>
2024-11-12 03:04:10 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Navbar from "@/components/Nav"
-import Hero from "@/components/Hero"
-import AboutMe from "@/components/AboutMe"
-import SkillshowCase  from "@/components/SkillshowCase"
-import Projects from "@/components/Projects"
-import Contact from "@/components/Contact"
+import Navbar from "../components/Nav"
+import Hero from "../components/Hero"
+import AboutMe from "../components/AboutMe"
+import SkillshowCase  from "../components/SkillshowCase"
+import Projects from "../components/Projects"
+import Contact from "../components/Contact"
 
 export default function Home(){
   return(
--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -8,12 +8,12 @@ const Aboutme = () => {
         About Me
         </h1>
         <p className='text-semibold'>
-            "My name is Alishba D/O Abdul Rahim. I am a passionate web developer with a knack for 
+            My name is Alishba D/O Abdul Rahim. I am a passionate web developer with a knack for 
             crafting innovative digital solutions and a love for coding. With expertise in front-end and back-end development
             I bring ideas to life. As a skilled web developer I combine creativity with technical
             expertise to deliver exceptional user experiences. Proficient in HTML CSS JavaScript and 
             frameworks like React and Next.js I excel at building scalable interactive web applications.
-            I can delivering high-quality solutions with precision and attention to detail."
+            I can delivering high-quality solutions with precision and attention to detail.
             
         </p>
 
